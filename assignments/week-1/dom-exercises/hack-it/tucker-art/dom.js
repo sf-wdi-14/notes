76f9632e165edbca7462e0ca42aabde6be28e264
Art Tucker <art.w.tucker@gmail.com>
2014-11-20 22:29:57 -0800

    Add DOM hack-it excercise for Art

---
+++ assignments/week-1/dom-exercises/hack-it/tucker-art/dom.js
@@ -0,0 +1,15 @@
+// http://www.businessinsider.com/
+
+var element1 = document.querySelector("div.id-546cc92569bedd6e1f52ca28 > h1 > a");
+element1.textContent = "'Every step you take, every move you make...' warns Uber CEO";
+
+var element2 = document.querySelector("div.id-546e8ae1eab8ea9f7a708c84 > h2 > a");
+element2.textContent="Obama declares GOP 'just a bunch of wrinkly-faced, sourpuss honkies'";
+
+var element3 = document.querySelector("div.span9 > h3");
+element3.textContent = "Same Shit, Different Day"
+
+var element4 = document.querySelector("div.right-news > h3");
+element4.textContent = "Let Us Advertise All Over Your (In)Box";
+
+
