d5876d8ecab1940d64072586018d48c12580dcd4
Cho S. Kim <choskim.me@gmail.com>
2014-11-20 16:52:40 -0800

    Add complete solution for fix-my-dom in dom exercises

--- assignments/week-1/dom-exercises/fix-my-dom/dom.js
+++ assignments/week-1/dom-exercises/fix-my-dom/dom.js
@@ -1,4 +1,4 @@
 // write code here
 document.querySelector("li").textContent = "first";
-var li = document.querySelectorAll("li")[1];
-li.textContent = "second";
+document.querySelectorAll("li")[1].textContent = "second";
+document.querySelectorAll("li")[3].textContent = "fourth";
