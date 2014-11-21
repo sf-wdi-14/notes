f05bb597ff0da5e2c3f3d327744f77c62ba86d3c
Ian Schoonover <iantskon@gmail.com>
2014-11-21 00:46:21 +0000

    Add Fix My Dom and Traffic Lights Exercise Answers

--- assignments/week-1/dom-exercises/fix-my-dom/dom.js
+++ assignments/week-1/dom-exercises/fix-my-dom/dom.js
@@ -1,1 +1,7 @@
-// write code here
+var uList = document.querySelector("ul");
+var list = uList.getElementsByTagName('li');
+var numbers = ["first", "second", "third", "fourth"];
+for (var i = 0; i<= list.length; i++) {
+	list[i].textContent = numbers[i];
+}
+
--- assignments/week-1/dom-exercises/traffic-lights/dom.js
+++ assignments/week-1/dom-exercises/traffic-lights/dom.js
@@ -1,1 +1,48 @@
-// write code here
+document.querySelector("#light1").addEventListener("mouseover", function() {
+	document.querySelector("#light1").style.fill = "red";
+});
+document.querySelector("#light1").addEventListener("mouseout", function() {
+	document.querySelector("#light1").style.fill = "white";
+});
+
+document.querySelector("#light2").addEventListener("mouseover", function() {
+	document.querySelector("#light2").style.fill = "yellow";
+});
+document.querySelector("#light2").addEventListener("mouseout", function() {
+	document.querySelector("#light2").style.fill = "white";
+});
+
+document.querySelector("#light3").addEventListener("mouseover", function() {
+	document.querySelector("#light3").style.fill = "green";
+});
+document.querySelector("#light3").addEventListener("mouseout", function() {
+	document.querySelector("#light3").style.fill = "white";
+});
+
+
+// My Solution, Very Verbose
+
+// var light1 = document.getElementById("light1");
+// var light2 = document.getElementById("light2");
+// var light3 = document.getElementById("light3");
+
+// light1.addEventListener("mouseover", function(){
+//     document.getElementById("light1").style.fill = "green";
+// });
+// light1.addEventListener("mouseout", function(){
+//     document.getElementById("light1").style.fill = "white";
+// });
+
+// light2.addEventListener("mouseover", function(){
+//     document.getElementById("light2").style.fill = "yellow";
+// });
+// light2.addEventListener("mouseout", function(){
+//     document.getElementById("light2").style.fill = "white";
+// });
+
+// light3.addEventListener("mouseover", function(){
+//     document.getElementById("light3").style.fill = "red";
+// });
+// light3.addEventListener("mouseout", function(){
+//     document.getElementById("light3").style.fill = "white";
+// });
