0b29afcfccd61cf70255d1d594eef10015d6eef9
Benjamin Crane <crane.benjamin@gmail.com>
2014-11-21 00:31:29 +0000

    Adding index and style files to make dom.js work

--- assignments/week-1/dom-exercises/fix-my-dom/benCrane/dom.js
+++ assignments/week-1/dom-exercises/fix-my-dom/benCrane/dom.js
@@ -1,30 +1,9 @@
 // write code here
-/*
+
 document.querySelector("li").textContent = "first";
 
-
-
 li2 = document.querySelectorAll("li")[1];
-li2.textContent ="second"
+li2.textContent ="second";
 
-//allLI[0].
-//document.querySelector(allLI[1]).textContent = "second"
-var h1 = document.querySelector('h1');
-
-var myFunction = function() {
-		console.log("You clicked me");
-};
-
-h1.addEventListener("click",myFunction);
-
-h1.removeEventListener("click",myFunction);
-*/
-
-
-document.querySelectorAll("#light1").addeEventListener("mouseover",function() {
-	document.querySelector("#light1").setAttribute("fill","red")
-})
-document.querySelectorAll("#light1").addeEventListener("mouseout",function() {
-	document.querySelector("#light1").setAttribute("fill","white")
-})
-	
+li4 = document.querySelectorAll("li")[3];
+li4.textContent="fourth";
