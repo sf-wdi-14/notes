3b9098eb7da03ca6bd9af834fa2bb13fd7d4f42d
iliastsangaris <iliastsangaris@gmail.com>
2015-01-06 23:25:27 -0800

    rm lab work from master

--- lectures/week-6/_3_wednesday/dusk/lab/FirstRailsApp/config/environment.rb
+++
@@ -1,7 +0,0 @@
-# Load the Rails application.
-require File.expand_path('../application', __FILE__)
-
-# Initialize the Rails application.
-Rails.application.initialize!
-
-# config.frameworks -= [:active_record, :active_resource, :action_mailer]
