32889142b991884b24ca3b2ca84043fa766303db
iliastsangaris <iliastsangaris@gmail.com>
2015-01-07 19:04:43 -0800

    function squeeze

--- lectures/week-6/_4_thursday/dawn/squeeze-morning-exercise/src/squeeze.js
+++ lectures/week-6/_4_thursday/dawn/squeeze-morning-exercise/src/squeeze.js
@@ -1,12 +1,1 @@
-function squeeze(string) {
-  //Array that will store single letters
-  var splitString = string.split("");
-  var previousLetter = "";
-  var singleLettersCollection = splitString.filter(function(letter){
-    if(letter != previousLetter){
-      previousLetter = letter;
-      return letter;
-    }
-  });
-  return singleLettersCollection.join("");
-}
+// write the function squeeze that will pass the tests...
